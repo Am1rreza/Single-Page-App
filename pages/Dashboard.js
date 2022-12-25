e005c46193eee67e35951494102387d81fe00544
Amirreza <amirreza.m13825@gmail.com>
2022-12-25 19:10:59 +0330

    add lorem to pages

--- pages/Dashboard.js
+++ pages/Dashboard.js
@@ -1,7 +1,11 @@
 const Dashboard = () => {
-    return ( 
-       `<h1>Dashboard Page</h1>`
-     );
-}
- 
+  return `<h1>Dashboard Page</h1>
+       <p>
+       Lorem ipsum dolor sit amet, consectetur adipisicing elit. Veritatis aliquid quo
+ducimus commodi suscipit, velit quisquam ea. Suscipit ducimus dolorem sint,
+provident incidunt optio ea rerum accusantium, numquam facere inventore.
+       </p>
+       `;
+};
+
 export default Dashboard;
--- pages/Products.js
+++ pages/Products.js
@@ -1,7 +1,11 @@
 const Products = () => {
-    return ( 
-       `<h1>Products Page</h1>`
-     );
-}
- 
+  return `<h1>Products Page</h1>
+       <p>
+       Lorem ipsum dolor sit amet, consectetur adipisicing elit. Veritatis aliquid quo
+ducimus commodi suscipit, velit quisquam ea. Suscipit ducimus dolorem sint,
+provident incidunt optio ea rerum accusantium, numquam facere inventore.
+       </p>
+       `;
+};
+
 export default Products;
